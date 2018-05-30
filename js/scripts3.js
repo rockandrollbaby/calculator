45e18cc1b64e1104a1cf6501dee8a93aa4da1daf
cynthia smith and scott fraser <scott@>
2018-05-30 16:03:12 -0700

    began conversions

---
+++ js/scripts3.js
@@ -0,0 +1,13 @@
+
+  var cups = parseInt(prompt("How many cups do you have?"));
+  var oz = parseInt(prompt("How many oz do you have?"));
+
+  var resultCups = (oz * (1 / 8));
+  var resultOz = (cups * (8 / 1));
+
+  alert("This is how many cups you will need " + resultCups + ".");
+  alert("This is how many oz you will need " + resultOz + ".");
+  
+$(document).ready(function(){
+
+});
